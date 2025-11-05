587f41ba6e20cb28003514470e1689d26bbb1eaf
Rachit Rawat <rawatritik5670@gmail.com>
2025-11-05 21:24:22 +0530

    Remove console log for admin user
    Removed console log for admin user creation.

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ async function ensureAdmin() {
     if (!user) {
       const hashed = await bcrypt.hash(password, 10);
       user = await User.create({ name, email, password: hashed, college, isAdmin: true, isVerified: true });
-      console.log('Admin user created:', email);
+      
     } else if (!user.isAdmin) {
       user.isAdmin = true;
       await user.save();
